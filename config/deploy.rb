923aed7a614321620e671ff68aead7a3954dd709
minkyojeong <redwonder@naver.com>
2018-08-18 11:22:11 +0900

    배포환경 수정

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.11.0"
 
 set :application, 'foremessage_server'
-set :repo_url, 'https://github.com/ForeMessage/foremessage_server.git'
+set :repo_url, 'git@github.com:ForeMessage/foremessage_server.git'
 
 set :deploy_to, '/var/www/foremessage_server'
 set :bundle_flags, ""
@@ -14,6 +14,7 @@ set :passenger_restart_options,
     -> { "#{deploy_to} --ignore-app-not-running --rolling-restart" }
 
 set :linked_dirs, %w{log}
+append :linked_files, "config/master.key"
 
 namespace :deploy do
   desc 'Restart application'
