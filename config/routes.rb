b505a04b6fb69a20f6ad97c0100f66663203c8f1
전준호 <>
2018-09-03 14:35:25 +0900

    route method 변경.

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
       namespace :users do
         post 'sign_in' => 'sessions#sign_in'
         post 'sign_up' => 'registrations#sign_up'
-        post 'check_nickname' => 'confirmations#check_nickname'
         post 'verify_number' => 'confirmations#verify_phone_number'
-        post 'recovery_nickname' => 'recoveries#recovery_nickname'
-        post 'recovery_password' => 'recoveries#recovery_password'
-        post 'reset_password' => 'recoveries#reset_password'
+        put 'reset_password' => 'recoveries#reset_password'
+        get 'check_nickname' => 'confirmations#check_nickname'
+        get 'recovery_nickname' => 'recoveries#recovery_nickname'
+        get 'recovery_password' => 'recoveries#recovery_password'
       end
     end
   end
