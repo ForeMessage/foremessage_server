a44b1250aa3dd9ee2d801863a897dcfd8efb4df8
전준호 <>
2018-09-30 17:49:43 +0900

    Api Subdomain 추가를 위한 namespace 삭제 및 에러 페이지 커스터마이징.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, :defaults => {:format => :json} do
-    namespace :v1 do
+  scope module: :api do
+    namespace :v1, :defaults => {:format => :json} do
       namespace :users do
         post 'sign_in' => 'sessions#sign_in'
         post 'sign_up' => 'registrations#sign_up'
