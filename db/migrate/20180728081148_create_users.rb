8c8ab2b9d454065105cf860573fadf93230879ae
전준호 <>
2018-10-01 20:20:29 +0900

    birthday Datetime -> Date로 변경

--- db/migrate/20180728081148_create_users.rb
+++ db/migrate/20180728081148_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :password_digest
       t.string :phone_number
       t.string :name
-      t.datetime :birthday
+      t.date :birthday
 
       t.timestamps
     end
