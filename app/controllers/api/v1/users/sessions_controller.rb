30de07c6d49a428ace28684f369a66740e306022
전준호 <>
2018-10-06 16:43:47 +0900

    1. 로그인 할 때 디바이스 토큰 갱신(기존과 토큰이 다르면 갱신, 같으면 PASS) 2. 유저 생성 될 때 UserToken row도 생성

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -4,13 +4,15 @@ class Api::V1::Users::SessionsController < ApplicationController
     nickname = params[:nickname]
     password = params[:password]
 
-    user = User.find_by_nickname(nickname)
+    user = User.find_by(nickname: nickname)
 
     # 유저 닉네임을 못 찾을 때
     render json: { message: 'invalid nickname' }, status: :bad_request and return unless user.present?
 
     # 비밀번호가 맞지 않을 때
     render json: { message: 'invalid password' }, status: :unauthorized and return unless user.authenticate(password)
+
+    user.refresh_device_token(params[:device_token])
 
     # SUCCESS LOGIN
     render json: { message: 'success login' }, status: :ok
