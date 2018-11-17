4a3d302d73fb90aec62ed4649a973aed4be0119a
전준호 <>
2018-11-17 13:43:42 +0900

    REFACTOR_RESPONSE_JSON - JSON 포맷팅하는 concerns 모듈 추가 - 모든 API 렌더링 모듈로 수정 - 에러 로그 포맷팅 확립 - exceptions 추가

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,62 +4,72 @@ class Api::V1::UsersController < ApplicationController
 
   # 번호 인증
   def verify_number
+    raise Exceptions::ParameterMissingError.new(:phone_number) unless params[:phone_number].present?
+
     begin
       authenticate = PhoneAuthenticationService.new
 
       certification_number = authenticate.send_message(params[:phone_number])
     rescue => e
-      render json: { error: e }, status: :bad_request and return
+      error_response(status: :bad_request, message: e.message) and return
     end
 
-    render json: { certification_number: certification_number, message: 'SUCCESS' }, status: :ok
+    success_response(extra_parameters: { certification_number: certification_number })
   end
 
   # 유저 확인
   def check_user
-    phone_number = params[:phone_number]
-    device_id = params[:device_id]
+    raise Exceptions::ParameterMissingError.new(:phone_number) unless params[:phone_number].present?
+    raise Exceptions::ParameterMissingError.new(:device_id) unless params[:device_id].present?
 
     exist_user = false
 
-    user_find_by_phone_number = User.find_by(phone_number: phone_number)
+    user_find_by_phone_number = User.find_by(phone_number: params[:phone_number])
 
     if user_find_by_phone_number.present?
-      user_find_by_phone_number.device_id == device_id ? exist_user = true : update_phone_number(user_find_by_phone_number)
+      user_find_by_phone_number.device_id == params[:device_id] ? exist_user = true : update_phone_number(user_find_by_phone_number)
     end
 
-    render json: { exist_user: exist_user }, status: :ok
+    success_response(extra_parameters: { exist_user: exist_user })
   end
 
   def check_in
+    raise Exceptions::ParameterMissingError.new(:phone_number) unless params[:phone_number].present?
+    raise Exceptions::ParameterMissingError.new(:device_token) unless params[:device_token].present?
+
     user = User.find_by(phone_number: params[:phone_number])
 
     update_token(user, params[:device_token])
 
-    render json: { access_token: @auth_secret.create_token(user), refresh_token: user.token.refresh_token }, status: :ok
+    success_response(extra_parameters: { access_token: @auth_secret.create_token(user), refresh_token: user.token.refresh_token })
   end
 
   def sign_in
+    raise Exceptions::ParameterMissingError.new(:phone_number) unless params[:phone_number].present?
+    raise Exceptions::ParameterMissingError.new(:refresh_token) unless params[:refresh_token].present?
+
     user = User.find_by(phone_number: params[:phone_number])
 
     begin
       access_token = @auth_secret.check_refresh_token(user, params[:refresh_token])
 
-      render json: { access_token: access_token, refresh_token: params[:refresh_token] }, status: :ok
+      success_response(extra_parameters: { access_token: access_token, refresh_token: params[:refresh_token] })
     rescue => e
-      render json: { error: e }, status: :forbidden
+      error_response(status: :forbidden, message: e.message) and return
     end
   end
 
   def sign_up
+    raise Exceptions::ParameterMissingError.new(:device_token) unless params[:device_token].present?
+
     begin
       user = User.create(user_params)
 
       update_token(user, params[:device_token])
 
-      render json: { access_token: @auth_secret.create_token(user), refresh_token: user.token.refresh_token }, status: :created
+      success_response(extra_parameters: { access_token: @auth_secret.create_token(user), refresh_token: user.token.refresh_token }, status: :created)
     rescue => e
-      render json: { error: e }, status: :bad_request
+      error_response(status: :bad_request, message: e.message) and return
     end
   end
 
@@ -69,7 +79,11 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update_token(user, device_token)
-    user.token.update_tokens(device_token)
+    begin
+      user.token.update_tokens(device_token)
+    rescue => e
+      error_response(status: :bad_request, message: e.message) and return
+    end
   end
 
   def update_phone_number(user)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
+  include Response
+  include ExceptionHandling
   skip_before_action :verify_authenticity_token
 
   before_action :check_access_token
 
   def check_access_token
+    puts request.headers[:Authorization]
+    raise Exceptions::RequestHeaderMissingError.new(:Authorization) unless request.headers[:Authorization].present?
+
     auth_secret_service = AuthSecretService.new
 
     access_token = request.headers[:Authorization].gsub(/^Bearer /, '')
@@ -12,8 +17,9 @@ class ApplicationController < ActionController::Base
       session[:user_number] = body['user_phone_number']
     rescue AuthSecretService::TokenExpiredError => e
       render :json => { result: e }, status: :unauthorized
+      error_response(status: :unauthorized, message: e.message) and return
     rescue AuthSecretService::TokenInvalidError => e
-      render :json => { result: e }, status: :unauthorized
+      error_response(status: :unauthorized, message: e.message) and return
     end
   end
 end
