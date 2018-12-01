b6861066467e29f12e212d451a6d93c5fda07fce
전준호 <>
2018-12-02 01:11:49 +0900

    FEATURE ADD_IMGAE_IN_MESSAGE - 메시지 보낼 때 이미지를 첨부하여 보낼 수 있도록 하다. - 캡스톤 템프 메시지 보내는 API 추가.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1, :defaults => {:format => :json} do
       scope :users do
         controller :users do
-          # post 'verify_number' => :verify_number
+          post 'verify_number' => :verify_number
           post 'check_in' => :check_in
           post 'sign_in' => :sign_in
           post 'sign_up' => :sign_up
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
       scope :message do
         controller :message do
           post 'send' => :send_message
+          post 'send_secret' => :send_secret
         end
       end
 
