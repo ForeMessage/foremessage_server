6e1318e8266bbbaf1ca9dc66a2c8fbbf799886b7
전준호 <>
2018-12-03 01:16:51 +0900

    이미지 JSON 파싱.

--- app/controllers/api/v1/message_controller.rb
+++ app/controllers/api/v1/message_controller.rb
@@ -21,26 +21,8 @@ class Api::V1::MessageController < ApplicationController
       message_info.merge!({ send_at: params[:send_at] }) if params[:send_at].present?
 
       if params[:image].present?
-
-        begin
-          temp_image = StringIO.new(Base64.decode64(params[:image].tr(' ', '+')))
-
-          temp_file = Tempfile.new(['hello', '.png']) do |f|
-            f.write temp_image.to_s
-          end
-
-          # image = Magick::ImageList.new
-          # bin = File.open(temp_file, 'r'){ |file| file.read }
-          # img = image.from_blob(bin)
-
-          image = MiniMagick::Image.read(temp_image)
-          temp_file.close
-        rescue => e
-          logger.debug "message: #{e.message} class: #{temp_image.class}, methods: #{temp_image.methods}"
-          logger.debug "message: #{e.message} class: #{image.class}, methods: #{image.methods}"
-
-          error_response(message: 'FAIL', extra_parameters: { class: temp_image.class, methods: temp_image.methods }) and return
-        end
+        temp_image = StringIO.new(Base64.decode64(params[:image].tr(' ', '+')))
+        image = MiniMagick::Image.read(temp_image)
 
         message_info.merge!({ image: image })
       end
@@ -61,7 +43,8 @@ class Api::V1::MessageController < ApplicationController
 
     receiver = User.where(phone_number: receiver_array).first
 
-    image = MiniMagick::Image.open(params[:image].path)
+    temp_image = StringIO.new(Base64.decode64(params[:image].tr(' ', '+')))
+    image = MiniMagick::Image.read(temp_image)
     image.draw 'image Over 0,0 0,0 "foremessage_logo.png"'
 
     message_info = {
