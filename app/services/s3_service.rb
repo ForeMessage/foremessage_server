c8e5cb8f13b9f87f3b312586ba2a8c601563833c
전준호 <>
2018-12-03 17:31:06 +0900

    이미지 JSON 파싱.

--- app/services/s3_service.rb
+++ app/services/s3_service.rb
@@ -11,9 +11,10 @@ class S3Service
   end
 
   def upload_image(image)
-    obj = @bucket.object("uploads/#{image.data['baseName']}")
+    obj = @bucket.object("uploads/#{image.data['baseName']}t.png")
     obj.upload_file(File.open(image.tempfile), acl:'public-read')
 
+    logger.debug "#{image.data['baseName']}"
     obj.public_url
   end
 end
